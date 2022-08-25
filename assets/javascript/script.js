e199ae19b159d049047d6fd5f22bfeb707f23aee
DomoMyles <47869842+DomoMyles@users.noreply.github.com>
2022-08-25 16:12:57 -0400

    Update To Code

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -3,8 +3,9 @@ var ques = document.querySelector('#Start')
 var score = 0;
 var time = 60;
 var place = 0;
-sScreen = false;
-var QTY = 1;
+var sScreen = false;
+var currentindex = 0;
+var num = 0;
 
 //set the questions and answers in an array
 var Questions = [
@@ -14,24 +15,24 @@ var Questions = [
 		correct: "4"
 	},
 	{
-		text: "What college is this form",
-		choices: ["UNH", "PSU", "Harvard", "Caltec"],
-		correct: "UNH"
+		text: "Which is global and changable",
+		choices: ["var", "const", "function", "let"],
+		correct: "var"
 	},
 	{
-		text: "What color is the sky?",
-		choices: ["red", "blue", "orange", "pink"],
-		correct: "blue"
+		text: "Which of the folowing calls upon a function?",
+		choices: ["const", "var", "onclick", "<div>"],
+		correct: "onclick"
 	},
 	{
-		text: "45 + 55?",
-		choices: ["100", "105", "95", "64"],
-		correct: "100"
+		text: "Which brackets should you use for contents within a function?",
+		choices: ["[]", "{}", "()", "<>"],
+		correct: "{}"
 	},
 	{
-		text: "What is the most common eye color?",
-		choices: ["green", "blue", "brown", "pink"],
-		correct: "brown"
+		text: "What should a html document start with?",
+		choices: ["<!DOCTYPE html>", "<head>", "<header>", "<title>"],
+		correct: "<!DOCTYPE html>"
 	}
 ]
 //grab from local storage
@@ -53,16 +54,17 @@ start.addEventListener
 			function countDown() {
 				document.getElementById("time").innerHTML = --time;
 				if (time < 0) {
-					clearInterval(myTimer)
-					scoreScreen()
+					clearInterval(myTimer);
+					scoreScreen();
+					document.getElementById("right").textContent = "Ran Out of time."
 				}
 			}
 		}
-		function timeUp() {
-			console.log("GAME OVER")
-		}
+
 		clock()
 		nextQuestion();
+
+
 		//Remove start button after quiz starts
 		var elem = document.getElementById("Start");
 		elem.style.visibility = "hidden";
@@ -70,14 +72,12 @@ start.addEventListener
 	)
 
 
-
-
-
 // skip to the next question
 function nextQuestion() {
 	if (place < Questions.length) {
 		document.getElementById("questions").innerHTML = Questions[place].text
 		Questions[place].choices.forEach(function (i) {
+
 			//function i creats the choices for each button in the array
 			const button = document.createElement("li");
 			button.innerText = i;
@@ -89,24 +89,27 @@ function nextQuestion() {
 			button.style.fontSize = '20px'
 			button.style.width = '500px'
 		})
-	}
-	else {
-		//When it runs out of questions show score
-		function endGame() {
-			var flscore = Math.floor(score);
-			document.getElementById("questions").innerHTML = "";
-			const ccore = document.getElementById("Score").innerHTML = "score: " + String(flscore);
-			ccore.style.margin = '10px';
-			ccore.style.backgroundColor = 'green';
-			ccore.style.padding = '20px';
-			ccore.style.fontSize = '20px';
-			localStorage.setItem('Score', score)
-			clearInterval(myTimer)
-
+	} else
+		if (place >= 5) {
+			scoreScreen()
 		}
-	}
+
+		else {
+			//When it runs out of questions show score
+			function endGame() {
+				var flscore = Math.floor(score);
+				document.getElementById("questions").innerHTML = "";
+				const ccore = document.getElementById("Score").innerHTML = "score: " + String(flscore);
+				ccore.style.margin = '10px';
+				ccore.style.backgroundColor = 'green';
+				ccore.style.padding = '20px';
+				ccore.style.fontSize = '20px';
+				ccore.style.classList = 'none'
+				localStorage.setItem('Score', score)
+				clearInterval(myTimer)
+			}
+		}
 }
-console.log(QTY)
 //check if the things you click is correct
 function checkCorrect(event) {
 	var result = (event.target.textContent);
@@ -117,11 +120,11 @@ function checkCorrect(event) {
 		score++
 		document.getElementById("right").innerHTML = "Right!!"
 	}
+
 	//Events when answer is incorrect
 	else {
-		document.getElementById("right").innerHTML = "Wronge!!"
+		document.getElementById("right").innerHTML = "Wrong!!"
 		var rime = (time - 20)
-		console.log(rime)
 		time = rime
 
 	}
@@ -131,74 +134,96 @@ function checkCorrect(event) {
 	nextQuestion();
 }
 
-<<<<<<< HEAD
 //set up score screen(LS = local storage)
 let LS = document.getElementById("scoreScreen")
 LS.addEventListener('click', event => {
-	scoreScreen()
-	var sScreen = true
+	sScreen = true
+
 })
 
 
 ///Go to Scorre Screen!!!!!!!!!!!!!!!!!!!!!!!
-function scoreScreen(event) {
+
+
+function scoreScreen() {
 	if (sScreen = true) {
-		var QTY = window.localStorage.getItem('Qty')
+		//reset quiz variables
+		time = 0
+		const ccore = document.getElementById("Score").innerHTML = ("Your Score:  " + score)
+
+		//show list items enables
+		var ulist = document.getElementById("ul")
+		ulist.style.visibility = "visible"
+
+		//show scores button visible
+		var Entr = document.getElementById("userNamebtn")
+		Entr.style.visibility = 'visible';
+
+		//shows your score
+		var ull = document.getElementById("Start")
+		ull.style.visibility = "visible"
+
 		//set up the text on screen
+		var reslt = document.getElementById("Resulta")
+		reslt.style.visibility = 'visible';
+
+		//hide the start button
 		var elem = document.getElementById("Start");
-		document.getElementById("Resulta").style = "display: inline"
-		elem.style.display = 'none';
-		console.log("OPEN UP");
+		elem.style.visibility = 'hidden';
+
+		//show t4est button
+		var testbutton = document.getElementById("show")
+		testbutton.style.visibility = "shown"
 
 		//set View Scores button to Back to quiz button
 		var buttonChange = document.getElementById("scoreScreen")
-		changepage()
 		buttonChange.classList.add("inputBar")
 
 		//Get text box to appear
-		const input = document.getElementById("userName");
-		const inputbtn = document.getElementById("userNamebtn");
-		input.style.display = "inline"
-		inputbtn.style.display = "inline"
+		var input = document.getElementById("inputBar");
+		input.style.visibility = 'visible';
 
+		//get enter button to appear
+		var inputbtn = document.getElementById("userNamebtn");
+		inputbtn.style.visibility = 'visible';
+
+		//get other btuton appear
+		var inputbtn = document.getElementById("SSbuttons");
+		inputbtn.style.visibility = 'visible';
+
+		changepage()
 		//set all other text empty
 		var p = document.getElementById("questions").textContent = "Enter your name!";
 		document.getElementById("answers").textContent = "";
 		document.getElementById("Start").textContent = "";
 		document.getElementById("time").textContent = "";
-
-		//find out what play you are
-
-		//Enter puts data into Local Storage and puts you to scores
-		var RF = document.getElementById("userNamebtn");
-		RF.addEventListener('click', event => {
-			var name = document.getElementById('userName').value
-			window.localStorage.setItem('name' + QTY, name + "   Score:" + score)
-			displayScores()
-			console.log(name)
-
-
-			//increase the # for amount of Local storage saves
-
-		})
-		let MY = document.getElementById("show")
-		MY.addEventListener('click', event => {
-			allStorage()
-			function allStorage() {
-				var values = [],
-					keys = Object.keys(localStorage),
-					i = keys.length;
-				console.log(i)
-				while (i--) {
-					values.push(localStorage.getItem(keys[i]));
-				}
-
-				console.log(values)
-				window.localStorage.setItem('Qty', values)
-			}
-		})
 	}
 }
+//Enter puts data into Local Storage and puts you to scores
+
+function enter() {
+	var emptythis = document.getElementById("ul")
+	emptythis.textContent = ""
+	var name = document.getElementById('inputBar').value
+	window.localStorage.setItem(name, name + " Score:" + score)
+
+	allStorage()
+	function allStorage() {
+		var values = [],
+			keys = Object.keys(localStorage),
+			i = keys.length;
+		while (i--) {
+			values.push(localStorage.getItem(keys[i]));
+
+		}
+		num = values;
+		displayScores()
+	}
+}
+
+//increase the # for amount of Local storage saves
+
+
 
 
 
@@ -206,31 +231,58 @@ if (sScreen = true) {//reload page when go back to quiz
 	function changepage() {
 		var MP = document.getElementById("quizScreen")
 		MP.addEventListener('click', event => {
-			backQuiz()
 		})
 	}
 }
 
 function backQuiz() {
-	//resets the quiz layout
-	console.log("IsThis???")
-	document.getElementById("ul").style = "display: none"
-	var p = document.getElementById("questions").textContent = "Questions";
-	document.getElementById("answers").textContent = "";
-	document.getElementById("time").textContent = "time";
 
+	//reset vairables
+	score = 0 
+	//resets the quiz layout
+	var reslt = document.getElementById("Resulta")
+	reslt.style.visibility = 'hidden';
+
+	var ull = document.getElementById("ul")
+	ull.style.visibility = "hidden"
+	ull.textContent = ""
+
+	var ull = document.getElementById("Score")
+	ull.style.visibility = "hidden"
+
+	var questions = document.getElementById("questions");
+	questions.textContent = "Questions"
+
+	var answers = document.getElementById("answers")
+	answers.textContent = "";
+
+	var answers = document.getElementById("Resulta")
+	answers.style.visibility = "hidden";
+
+	var timee = document.getElementById("time").textContent = "time";
+	time.textContent = "time"
+
+	var elem = document.getElementById("Start");
+	elem.style.visibility = "visible";
+
+	
 
 	//Removes the textbar (if exists)
-	var elemtxtbar = document.getElementById("userName");
-	elemtxtbar.style.display = "none"
+	var elemtxtbar = document.getElementById("inputBar");
+	elemtxtbar.style.visibility = "hidden"
 	var elembtn = document.getElementById("userNamebtn");
-	elembtn.style.display = "none"
+	elembtn.style.visibility = "hidden"
 	var startBtn = document.getElementById("Start");
-	startBtn.style.display = "inline";
+	startBtn.style.visibility = "shown";
 	startBtn.textContent = "Start";
-	console.log("meee")
+
+	//stop timer
+	time = 0
+
 }
 
+
+var value = []
 //function to display the scores
 function displayScores() {
 	allStorage()
@@ -238,58 +290,69 @@ function displayScores() {
 		var values = [],
 			keys = Object.keys(localStorage),
 			i = keys.length;
-		console.log(i)
 		while (i--) {
 			values.push(localStorage.getItem(keys[i]));
+			value = values
 		}
-		
-		let KE = document.getElementById("refreshList")
-		KE.addEventListener('click', event => {
-			document.getElementById("userNamebtn").style.display = "inline"
-			document.getElementById("refreshList").style.display = "inline"
+	}
+	let kill = document.getElementById("refreshList")
+	kill.addEventListener('click', event => {
+		document.getElementById("ul").textContent = ""
+		localStorage.clear()
+	})
 
-			document.getElementById("Haa").remove()
-		})
 
-		console.log(values)
-		var sayScore = document.getElementById("questions");
-		sayScore.textContent = "LeaderBoard"
-		sayScore.style.display = "inline"
-		document.getElementById("userNamebtn").style.display = "inline"
-		document.getElementById("refreshList").style.display = "inline"
+	//set up leaderboard
+	var sayScore = document.getElementById("questions");
+	sayScore.textContent = "LeaderBoard"
+	sayScore.style.visibility = "shown"
+	document.getElementById("userNamebtn").style.visibility = "shown"
+	document.getElementById("refreshList").style.visibility = "shown"
 
-		
+	//show refresh buton
+	var refr = document.getElementById("show")
+	refr.style.visibility = "shown"
 
-		//set loop too make things
-		for (let r = 0; r < values.length; r++){
+	//set loop too make things
+	for (let i = 0; i < value.length; i++) {
+		var list = document.getElementById("ul")
+		list.style.visibility = "shown"
 		var LBscores = document.createElement("li")
-		LBscores.textContent = (values[r])
+		LBscores.textContent = value[i]
 		LBscores.setAttribute("id", "Haa")
-		Resulta.append(LBscores);
-		console.log(r)
+		ul.append(LBscores);
+	}
+}
 
+
+
+
+
+let clearStorageExcept = function (exceptions) {
+	let keys = [];
+	exceptions = [].concat(exceptions); // prevent undefined
+
+	// get storage keys
+	$.each(localStorage, (key) => {
+		keys.push(key);
+	});
+
+	// loop through keys
+	for (let i = 0; i < keys.length; i++) {
+		let key = keys[i];
+		let deleteItem = true;
+
+		// check if key excluded
+		for (let j = 0; j < exceptions.length; j++) {
+			let exception = exceptions[j];
+			if (key == exception) {
+				deleteItem = false;
+			}
 		}
 
+		// delete key
+		if (deleteItem) {
+			localStorage.removeItem(key);
+		}
 	}
-}
-//value that will calculate how many list items there are
-function P()
-document.ready(function(){
-	P("Resulta").click(function(){
-	   P("#li").remove();
-	});
- });
-
-
-///clear local storage
-function clear() {
-	localStorage.clear();
-	console.log("wee")
-	console.log(buttonChange)
-}
-=======
-
-function reset(){
-	location.reload();
-	}
->>>>>>> 4b32e015735ad3f9d0979eda5cc507c2fb924135
+};
